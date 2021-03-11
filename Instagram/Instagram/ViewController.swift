e552a4c55815760248018632ef0dbbbd1aa0b5f0
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-03-11 14:06:55 +0900

    Scroll Work 0(init)

--- Instagram/Instagram/ViewController.swift
+++ Instagram/Instagram/ViewController.swift
@@ -18,26 +18,39 @@ class ViewController: UIViewController {
     }()
     
 // snapKit prepare
-    lazy var titleBox = { () -> UILabel in
-       let view = UILabel()
+// 인스타그램 타이틀 입력
+    lazy var titleBox = { () -> UIView in
+       let view = UIView()
         view.backgroundColor = .blue
         return view
     }()
-    
-    lazy var storyBox = { () -> UILabel in
-        let view = UILabel()
+// 그림 로고를 넣는 방법이 있었지... 굳이 글자에 폰트 안넣고...
+//    lazy var InstagramTxt = { () -> UILabel in
+//        let view = UILabel()
+//        view.text = "Instagram"
+//        view.textColor = .black
+//        view.font = .boldSystemFont(ofSize: 20)
+//        view.font = UIFont(name: , size: 15)
+//        return view
+//    }()
+// 스토리 나오는 공간
+    lazy var storyBox = { () -> UIView in
+        let view = UIView()
         view.backgroundColor = .gray
         return view
     }()
     
-    lazy var contentBox = { () -> UILabel in
-        let view = UILabel()
-        view.backgroundColor = .darkGray
-        return view
-    }()
+// 콘텐츠 나오는 공간
+    let contentBox = UIScrollView()
+    let contentView = UIView()
+    let content1 = UIView()
+    let content2 = UIView()
+    let content3 = UIView()
+    let content4 = UIView()
+    let content5 = UIView()
     
-    lazy var pageBox = { () -> UILabel in
-        let view = UILabel()
+    lazy var pageBox = { () -> UIView in
+        let view = UIView()
         view.backgroundColor = .red
         return view
     }()
@@ -45,39 +58,104 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        self.view.addSubview(Background)
-        
+
+// TitleBox WORKSPACE
         self.view.addSubview(titleBox)
-        self.view.addSubview(storyBox)
-        self.view.addSubview(contentBox)
-        self.view.addSubview(pageBox)
-        
-        Background.snp.makeConstraints { (make) in
-            make.edges.equalTo(self.view)
-        }
-        
-// snapKit workspace
         titleBox.snp.makeConstraints { (make) in
-            make.width.equalTo(Background.snp.height)
-            make.height.equalTo(70)
+            make.top.equalToSuperview().offset(35)
+            make.width.equalToSuperview()
+            make.height.equalTo(40)
         }
-        
+//        titleBox.addSubview(InstagramTxt)
+//        InstagramTxt.snp.makeConstraints { (make) in
+//            make.width.equalToSuperview().dividedBy(1.1)
+//            make.centerX.top.bottom.equalToSuperview()
+//        }
+// Story Scroll WORKSPACE
+        self.view.addSubview(storyBox)
         storyBox.snp.makeConstraints { (make) in
-            make.width.height.equalTo(titleBox)
-            make.top.equalTo(titleBox.snp.bottom)
+            make.top.equalTo(titleBox.snp.bottom).offset(5)
+            make.width.equalToSuperview()
+            make.height.equalTo(60)
         }
+
         
+// Content Scroll WORKSPACE
+        self.view.addSubview(contentBox)
         contentBox.snp.makeConstraints { (make) in
-            make.width.equalTo(titleBox.snp.width)
-            make.height.equalTo(Background.snp.height).dividedBy(1.7)
-            make.top.equalTo(storyBox.snp.bottom)
+            make.top.equalTo(storyBox.snp.bottom).offset(5)
+            make.height.equalTo(700)
+            make.width.equalToSuperview()
         }
         
-        pageBox.snp.makeConstraints { (make) in
-            make.width.height.equalTo(titleBox)
-            make.top.equalTo(contentBox.snp.bottom)
+        contentBox.addSubview(contentView)
+        contentView.snp.makeConstraints { (make) in
+            make.width.equalToSuperview()
+            make.centerX.top.bottom.equalToSuperview()
         }
+        
+        _ = [content1,content2,content3,content4,content5].map { self.contentView.addSubview($0) }
+        
+        content1.backgroundColor = .red
+        content2.backgroundColor = .blue
+        content3.backgroundColor = .gray
+        content4.backgroundColor = .darkGray
+        content5.backgroundColor = .lightGray
+
+        
+        content1.snp.makeConstraints { (make) in
+            make.leading.top.trailing.equalToSuperview()
+            make.height.equalTo(250)
+        }
+        
+        content2.snp.makeConstraints { (make) in
+            make.top.equalTo(content1.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(250)
+        }
+        
+        content3.snp.makeConstraints { (make) in
+            make.top.equalTo(content2.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(250)
+        }
+        
+        content4.snp.makeConstraints { (make) in
+            make.top.equalTo(content3.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(250)
+        }
+        
+        content5.snp.makeConstraints { (make) in
+            make.top.equalTo(content4.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(250)
+            make.bottom.equalToSuperview()
+        }
+//
+//        self.view.addSubview(Background)
+//
+//        self.view.addSubview(pageBox)
+//
+//        Background.snp.makeConstraints { (make) in
+//            make.edges.equalTo(self.view)
+//        }
+//
+//// snapKit workspace
+//        titleBox.snp.makeConstraints { (make) in
+//            make.width.equalTo(Background.snp.height)
+//            make.height.equalTo(70)
+//        }
+//
+//        storyBox.snp.makeConstraints { (make) in
+//            make.width.height.equalTo(titleBox)
+//            make.top.equalTo(titleBox.snp.bottom)
+//        }
+//
+//        pageBox.snp.makeConstraints { (make) in
+//            make.width.height.equalTo(titleBox)
+//            make.top.equalTo(contentBox.snp.bottom)
+//        }
     }
 
 }
