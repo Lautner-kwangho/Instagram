1dcc372519e1e957d0140af9157cf327a6139f29
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-03-16 01:31:45 +0900

    TableView Initial Setting

--- Instagram/Instagram_2/Instagram_2/ViewController.swift
+++ Instagram/Instagram_2/Instagram_2/ViewController.swift
@@ -8,15 +8,95 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+    let myTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .gray
+        tableView.separatorColor = .black
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    let item = ["1","2","3","4","5"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupTableView()
     }
     
+    func setupTableView() {
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        view.addSubview(myTableView)
+        
+        myTableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        myTableView.register(instagramContentCell.self, forCellReuseIdentifier: "Cell")
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        title = "Instagram"
+        return title
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! instagramContentCell
+        cell.backgroundColor = .white
+        cell.contentCell.text = "Day \(indexPath.row + 1)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
+}
+
+class instagramContentCell: UITableViewCell {
+    
+    let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    let contentCell: UILabel = {
+       let label = UILabel()
+        label.text = "Day 1"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.backgroundColor = .white
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        superCellView()
+    }
+    
+    func superCellView() {
+        addSubview(cellView)
+        cellView.addSubview(contentCell)
+        
+        cellView.snp.makeConstraints { (make) in
+            make.edges.lessThanOrEqualToSuperview().offset(30)
+        }
+        contentCell.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 #if DEBUG
