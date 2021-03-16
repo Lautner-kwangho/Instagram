d51d33ed88f78db366e4bfe35f9f73c46046bee2
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-03-17 02:29:50 +0900

    upload png file & tableView work

--- Instagram/Instagram_2/Instagram_2/ViewController.swift
+++ Instagram/Instagram_2/Instagram_2/ViewController.swift
@@ -9,16 +9,15 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let userName = ["경희", "연세", "한양", "서울", "경기", "수원"]
     
     let myTableView: UITableView = {
         let tableView = UITableView()
-        tableView.backgroundColor = .gray
+        tableView.backgroundColor = .white
         tableView.separatorColor = .black
-        tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-    let item = ["1","2","3","4","5"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,7 +29,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         myTableView.delegate = self
         myTableView.dataSource = self
-        view.addSubview(myTableView)
+        self.view.addSubview(myTableView)
         
         myTableView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
@@ -39,23 +38,93 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return item.count
+        return userName.count
     }
 
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        title = "Instagram"
-        return title
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let HeaderStoryVew: UIView = {
+            let view = UIView()
+            view.backgroundColor = .white
+            return view
+        }()
+        
+        let btnInstagram: UIButton = {
+            let btn = UIButton()
+            btn.setImage(UIImage(named: "InstagramLogo.png"), for: .normal)
+            return btn
+        }()
+        
+        let btnUploadStory: UIButton = {
+            let btn = UIButton()
+            btn.setImage(UIImage(named: "comment.png"), for: .normal)
+            return btn
+        }()
+        
+        let btnCheckFeed: UIButton = {
+            let btn = UIButton()
+            btn.setImage(UIImage(named: "upload.png"), for: .normal)
+            return btn
+        }()
+        
+        let btnSendMessage: UIButton = {
+            let btn = UIButton()
+            btn.setImage(UIImage(named: "Message.png"), for: .normal)
+            return btn
+        }()
+    
+        HeaderStoryVew.addSubview(btnInstagram)
+        HeaderStoryVew.addSubview(btnUploadStory)
+        HeaderStoryVew.addSubview(btnCheckFeed)
+        HeaderStoryVew.addSubview(btnSendMessage)
+        
+        btnInstagram.snp.makeConstraints { (make) in
+            make.width.equalTo(130)
+            make.height.equalTo(40)
+            make.centerY.equalTo(HeaderStoryVew.snp.centerY)
+            make.left.equalTo(HeaderStoryVew.snp.left)
+        }
+        btnUploadStory.snp.makeConstraints { (make) in
+            make.width.equalTo(35)
+            make.height.equalTo(35)
+            make.centerY.equalTo(HeaderStoryVew.snp.centerY).offset(-1)
+            make.right.equalTo(btnCheckFeed.snp.left).offset(-20)
+        }
+        btnCheckFeed.snp.makeConstraints { (make) in
+            make.width.equalTo(40)
+            make.height.equalTo(40)
+            make.centerY.equalTo(HeaderStoryVew.snp.centerY)
+            make.right.equalTo(btnSendMessage.snp.left).offset(-15)
+        }
+        btnSendMessage.snp.makeConstraints { (make) in
+            make.width.equalTo(35)
+            make.height.equalTo(35)
+            make.centerY.equalTo(HeaderStoryVew.snp.centerY).offset(-2)
+            make.right.equalTo(HeaderStoryVew.snp.right).offset(-20)
+        }
+        return HeaderStoryVew
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! instagramContentCell
         cell.backgroundColor = .white
-        cell.contentCell.text = "Day \(indexPath.row + 1)"
+        cell.contentCell.text = userName[indexPath.row]
+        
         return cell
     }
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 400
+        return 700
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        title = "home / uploading / story / profile / setting"
+        return title
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 50
     }
 }
 
@@ -69,11 +138,19 @@ class instagramContentCell: UITableViewCell {
     
     let contentCell: UILabel = {
        let label = UILabel()
-        label.text = "Day 1"
+        label.text = "userName"
         label.textColor = .black
         label.font = UIFont.boldSystemFont(ofSize: 20)
         label.backgroundColor = .white
         return label
+    }()
+    
+    let pictureCell: UIImageView = {
+        var Image = UIImageView()
+        Image.backgroundColor = .white
+        Image.image = UIImage(named: "tiger.jpg")
+        Image.contentMode = .scaleAspectFit
+        return Image
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -87,10 +164,15 @@ class instagramContentCell: UITableViewCell {
         cellView.addSubview(contentCell)
         
         cellView.snp.makeConstraints { (make) in
-            make.edges.lessThanOrEqualToSuperview().offset(30)
+            make.size.equalToSuperview()
         }
         contentCell.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
+            make.size.equalTo(cellView.snp.size).dividedBy(0.5)
+        }
+        
+        cellView.addSubview(pictureCell)
+        pictureCell.snp.makeConstraints { (make) in
+            make.size.equalTo(cellView.snp.size).dividedBy(0.5)
         }
     }
     
